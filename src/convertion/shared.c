e3e89aceba2b1d32606e18a1a2e499784f4ead84
Spare Lisa <sparelis@vo-i3.msk.21-school.ru>
2022-06-07 17:19:01 +0300

    tests init

--- src/convertion/shared.c
+++ src/convertion/shared.c
@@ -1,7 +1,7 @@
 #include "decimal.h"
 
 void change_endian(s21_decimal *my_struct) {
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < 4; i++) {
     int temp = 0;
     for (int j = 0, k = 31; j <= 31; j++, k--) {
       if (my_struct->bits[i] & (1 << j)) {
