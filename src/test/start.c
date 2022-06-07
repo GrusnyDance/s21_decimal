dc294009486c6b0b5941b0f25ada0e97e8208f27
GrusnyDance <grusny.dance@gmail.com>
2022-06-08 01:34:39 +0200

    gmp lib compiles

--- src/test/start.c
+++ src/test/start.c
@@ -1,62 +1,26 @@
 // gcc -o test test.c $(pkg-config --libs --cflags gmp)
-// -I/usr/local/Cellar/gmp/6.2.1_1/include -L/usr/local/Cellar/gmp/6.2.1_1/lib -lgmp
-#include <stdio.h>
+// -I/usr/local/Cellar/gmp/6.2.1_1/include -L/usr/local/Cellar/gmp/6.2.1_1/lib
+// -lgmp
+#if defined(__linux__)
+#include "./gmp-6.2.1/gmp.h"
+#else
 #include <gmp.h>
+#endif
+#include <stdio.h>
 
-int main(){
-
-  char inputStr[1024];
-  /*
-     mpz_t is the type defined for GMP integers.
-     It is a pointer to the internals of the GMP integer data structure
-   */
-  mpz_t n;
-  int flag;
-
-  printf ("Enter your number: ");
-  scanf("%1023s" , inputStr); /* NOTE: never every write a call scanf ("%s", inputStr);
-                                  You are leaving a security hole in your code. */
-
-  /* 1. Initialize the number */
-  mpz_init(n);
-  mpz_set_ui(n,0);
-
-  /* 2. Parse the input string as a base 10 number */
-  flag = mpz_set_str(n,inputStr, 10);
-  assert (flag == 0); /* If flag is not 0 then the operation failed */
-
-  /* Print n */
-  printf ("n = ");
-  mpz_out_str(stdout,10,n);
-  printf ("\n");
-
-  /* 3. Add one to the number */
-
-  mpz_add_ui(n,n,1); /* n = n + 1 */
-
-  /* 4. Print the result */
-
-  printf (" n +1 = ");
-  mpz_out_str(stdout,10,n);
-  printf ("\n");
-
-
-  /* 5. Square n+1 */
-
-  mpz_mul(n,n,n); /* n = n * n */
-
-
-  printf (" (n +1)^2 = ");
-  mpz_out_str(stdout,10,n);
-  printf ("\n");
-
-
-  /* 6. Clean up the mpz_t handles or else we will leak memory */
-  mpz_clear(n);
-
+int main() {
+  mpz_t r, n, m;
+  mpz_inits(r, n, m);   // очищается только то что инициализировано
+  mpz_set_ui(n, 0b0000000000001);
+  mpz_set_ui(m, 3);
+  mpz_add(r, n, m);
+  gmp_printf("%Zd\n", r);
+  mpz_clears(r);
+  return 0;
 }
-
 
 // https://home.cs.colorado.edu/~srirams/courses/csci2824-spr14/gmpTutorial.html
 // https://gmplib.org/manual/Integer-Arithmetic#Integer-Arithmetic
 // https://gmplib.org/gmp-man-6.2.1.pdf
+// https://www.mitchr.me/SS/exampleCode/GMP.html
+// http://cuiaoxiang.github.io/2013/10/02/gmp-introduction/
