545836a1b4c1b41551366302b28ce0a5a7a566e4
Spare Lisa <sparelis@vo-i2.msk.21-school.ru>
2022-06-08 18:21:52 +0300

    test generates random numbers

--- src/test/start.c
+++ src/test/start.c
@@ -7,20 +7,46 @@
 #include <gmp.h>
 #endif
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void print_bits(mpz_t var);
 
 int main() {
-  mpz_t r, n, m;
-  mpz_inits(r, n, m);   // очищается только то что инициализировано
-  mpz_set_ui(n, 0b0000000000001);
-  mpz_set_ui(m, 3);
-  mpz_add(r, n, m);
-  gmp_printf("%Zd\n", r);
-  mpz_clears(r);
+  gmp_randstate_t rstate;
+  gmp_randinit_default(rstate);
+  gmp_randseed_ui(rstate, time(NULL));
+  srand(time(NULL));
+  mpz_t n, m;
+  int two_pow;
+  mpz_init2(m, 128);
+  mpz_init2(n, 128);  // очищается только то что инициализировано
+  for (int i = 0; i < 10; i++) {
+    two_pow = rand() % 96;
+    mpz_ui_pow_ui(n, 2, two_pow);
+    mpz_urandomm(m, rstate, n);  // range 2n-1 to 2n-1, inclusive
+    print_bits(m);
+  }
+  gmp_randclear(rstate);
+  mpz_clear(n);
   return 0;
 }
 
-// https://home.cs.colorado.edu/~srirams/courses/csci2824-spr14/gmpTutorial.html
-// https://gmplib.org/manual/Integer-Arithmetic#Integer-Arithmetic
-// https://gmplib.org/gmp-man-6.2.1.pdf
-// https://www.mitchr.me/SS/exampleCode/GMP.html
-// http://cuiaoxiang.github.io/2013/10/02/gmp-introduction/
+void print_bits(mpz_t var) {
+  mpz_t tmp, res;
+  mpz_init2(res, 128);
+  mpz_init2(tmp, 128);
+  mpz_ui_pow_ui(tmp, 2, 127);
+  gmp_printf("\n%Zd\n", tmp);
+  for (int i = 127; i >= 0; i--) {
+    mpz_ui_pow_ui(tmp, 2, i);
+    mpz_and(res, tmp, var);
+    if (mpz_cmp_ui(res, 0)) {
+      printf("\033[33m1\033[0m");
+    } else {
+      printf("0");
+    }
+  }
+  mpz_clear(tmp);
+  mpz_clear(res);
+}
