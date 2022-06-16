699c55afd09214b292ce7be7b317faae5ad0fca0
_co_cy_ <stf2003.kem@gmail.com>
2022-06-17 00:48:30 +0300

    Updated debug print decimal

--- conv.py
+++ conv.py
@@ -1,13 +1,7 @@
-line = input().replace('-', ':')
-line = line.strip("-:")
+from custom_decimal import MyDecimal
 
-g = line.split(':')
-sys = g[0]
-sign = -1 if g[0][0] == '1' else 1
-exponent = 10 ** int(g[0][1:].replace('O', ''), base=2)
-number = int(''.join(g[1:]), base=2)
-# print()
-print("\tCODE NUMBER:\t\t", sign * number)
-print("\tEXPONENT:\t\t\t", exponent)
-print("\tFINALY:\t\t\t\t%0.28f"%(sign * number / exponent))
+decimal = MyDecimal(input().strip('\" '))
+
+print("\tEXPONENT:\t\t\t", decimal.exponent)
+print("\tFINALY:\t\t\t\t%0.28f"%decimal.decimal)
 # print()
--- src/another/balancing.c
+++ src/another/balancing.c
@@ -5,47 +5,48 @@
 #include "decimal_another.h"
 
 int shifting(s21_decimal *a, int n) {
-  /// Number shift by 10
-  s21_decimal bobka;
+    /// Number shift by 10
+    s21_decimal bobka;
     s21_from_int_to_decimal(10, &bobka);
     set_exponent(&bobka, 0);
-  s21_decimal tmp = init_zero_decimal();
-  int exp = get_exponent(*a);
+    s21_decimal tmp = init_zero_decimal();
+    int exp = get_exponent(*a);
 
-  int status = exp == MAX_EXPONENT;
-  // if (!status) {
-  //   exp++;
-  //   *a = tmp;
-  // }
-  while (n-- && exp < MAX_EXPONENT && !status) {
-    status = stupid_mul(*a, bobka, &tmp);
-    // d_print_decimal(tmp);
-    if (!status) {
-      *a = tmp;
-      tmp = init_zero_decimal();
-      exp++;
+    int status = exp == MAX_EXPONENT;
+    // if (!status) {
+    //   exp++;
+    //   *a = tmp;
+    // }
+    while (n-- && exp < MAX_EXPONENT && !status) {
+        status = stupid_mul(*a, bobka, &tmp);
+        // d_print_decimal(tmp);
+        if (!status) {
+            *a = tmp;
+            d_print_decimal(tmp);
+            tmp = init_zero_decimal();
+            exp++;
+        }
     }
-  }
-  set_exponent(a, exp);
-  return status;
+    set_exponent(a, exp);
+    return status;
 }
 
 int balancing(s21_decimal *a, s21_decimal *b) {
-  int status = 0;
-  int diff = get_exponent(*a) - get_exponent(*b);
+    int status = 0;
+    int diff = get_exponent(*a) - get_exponent(*b);
 
-  if (diff) {
-    if (diff > 0)
-      shifting(b, diff);
-    else
-      shifting(a, -diff);
-    diff = get_exponent(*a) - get_exponent(*b);
     if (diff) {
-      if (diff > 0)
-        bank_round(a, diff);
-      else
-        bank_round(b, -diff);
+        if (diff > 0)
+            shifting(b, diff);
+        else
+            shifting(a, -diff);
+        diff = get_exponent(*a) - get_exponent(*b);
+        if (diff) {
+            if (diff > 0)
+                bank_round(a, diff);
+            else
+                bank_round(b, -diff);
+        }
     }
-  }
-  return status;
+    return status;
 }
