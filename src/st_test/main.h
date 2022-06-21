9cbb0313d4ee5c6d225f682a669266ecf1b8598a
nanadaug <nanadaug@student.21-school.ru>
2022-06-21 14:28:25 +0300

    Added test for dec_to_float

--- src/st_test/main.h
+++ src/st_test/main.h
@@ -11,6 +11,7 @@
 #define ROUND_PATH " py_test/print_round_testcase.py"
 #define FLOOR_PATH " py_test/print_floor_testcase.py"
 #define NEGATE_PATH " py_test/print_negate_testcase.py"
+#define DEC_TO_FLOAT_PATH " py_test/print_dec_to_float_testcase.py"
 
 #define REDIRECT " >> "
 #define REDIRECT_FILE "input"
@@ -20,6 +21,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+
+#define EPS 1e-7
 
 static long long cur_testcase = 0;
 static char ** numbers;
@@ -33,9 +36,13 @@ void generate_testcase(int count, char *command_path);
 void free_testcase(int count);
 void get_testcase(char **number, char **result);
 
+// another
 void run_truncate_test(int count);
 void run_round_test(int count);
 void run_floor_test(int count);
 void run_negate_test(int count);
+
+// conversion
+void run_dec_to_float_test(int count);
 
 #endif //S21_DECIMAL_MAIN_H
