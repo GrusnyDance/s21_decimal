d0b6aa46f37dfcd82335381afbdb0307ff1ed578
nanadaug <nanadaug@student.21-school.ru>
2022-06-21 11:17:29 +0300

    Сode review

--- src/another/s21_round.c
+++ src/another/s21_round.c
@@ -1,12 +1,17 @@
 #include "decimal_another.h"
 
 int s21_round(s21_decimal value, s21_decimal *result) {
-  s21_decimal zeroDotFive = {{5, 0, 0, 0}}, mod_res, one = {{1, 0, 0, 0}};
-  s21_mod(value, one, &mod_res);
-  s21_truncate(value, result);
-  set_exponent(&zeroDotFive, 1);
-  set_sign(&mod_res, 0);
-  if (!s21_is_less(mod_res, zeroDotFive))
-    very_stupid_add(*result, one, result, 0, 0);
-  return 0;
+    s21_decimal one, zeroDotFive, mod_res;
+
+    s21_from_int_to_decimal(1, &one);
+    s21_from_int_to_decimal(5, &zeroDotFive);
+    set_exponent(&zeroDotFive, 1);
+
+    s21_mod(value, one, &mod_res);
+    set_sign(&mod_res, 0);
+
+    s21_truncate(value, result);
+    if (!s21_is_less(mod_res, zeroDotFive))
+        very_stupid_add(*result, one, result, 0, 0);
+  return OK;
 }
